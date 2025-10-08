64b7d11b6c4ba660e3d246a33bbe04a6b4aa0176
cauaojordao <cauaojordao@gmail.com>
2025-10-07 21:32:30 -0300

    ft: criado o exemplo didático de paralelismo com threads

---
+++ Threads/Program.cs
@@ -0,0 +1,16 @@
+﻿Thread t1 = new Thread(Trabalho);
+Thread t2 = new Thread(Trabalho);
+
+t1.Start("Thread 1");
+t2.Start("Thread 2");
+
+Console.WriteLine("Threads iniciadas.");
+
+void Trabalho(object nome)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{nome} executando passo {i + 1}");
+        Thread.Sleep(500);
+    }
+}
