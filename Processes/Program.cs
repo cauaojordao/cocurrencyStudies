2fe7ce0480c2f44c817af76d95245c892270f65d
cauaojordao <cauaojordao@gmail.com>
2025-10-07 21:30:48 -0300

    ft: criado o exemplo didático de processos

---
+++ Processes/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("=== Processos Ativos ===");
+foreach (var proc in Process.GetProcesses())
+{
+    Console.WriteLine($"{proc.Id} - {proc.ProcessName}, {proc.Threads.Count} threads");
+}
